49f100d79089be6fe211abb359ae8775b279cc79
Raymond BBF <ketchup942@gmail.com>
2021-03-03 15:08:49 +0800

    New Word

---
+++ degree.py
@@ -0,0 +1,3 @@
+degree_c = x = int(input("請輸入攝氏:"))
+degree_f = y = x*1.8+32
+print("華氏為:", y, "°F")
---
+++ if0.py
@@ -0,0 +1,19 @@
+weight = int(input("請輸入體重"))
+
+print("體重", weight, )
+x = weight
+if x == 100:
+	print("也太剛好了八")
+if x > 100:
+	print("該減肥了")
+if x < 100:
+	print("很棒喔繼續維持")
+
+
+rain=input("今天有天晴嗎")
+if rain == "有":
+	print("可以吃炒烏龍麵")
+	print("可以逛夜市")
+if rain != "有":
+	print("在家看netflix")
+	print("打R6")

66c70eea30778d5d1477caeaa3618673fc723942
Raymond BBF <ketchup942@gmail.com>
2021-03-04 19:52:12 +0800

    'faster_find_what_you_want'

--- rw2.py
+++ rw2.py
@@ -12,3 +12,19 @@ for count_1 in data:
 	number = number + len(count_1)
 print("每行資料平均為", number/len(data), "個字")
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(data)
+print("總共有", len(new), "筆資料")
+
+good = [] # 設一個資料夾為good
+for e in data: # 從data中找出我要的資料e
+	if "good" in e: # 如果我找到的資料中有字串good
+		good.append(data) # 把找到的字串放入設的資料夾good中
+print("總共有",len(good),"好的留言")
+
+good = [e for e in data if "good" in e] # 設立一個Good的資料夾,把有good留言的各條資料從data中找出
+print(len(good))
+new = [d for d in data if len(d) < 100] # 設立一個New的資料夾,把所有留言少於100字的資料找出來
+print(len(new))

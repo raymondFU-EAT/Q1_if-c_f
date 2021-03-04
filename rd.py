e5a8ca871b3fbaf3ed252b4603d3db72dd3dde95
Raymond BBF <ketchup942@gmail.com>
2021-03-04 14:34:20 +0800

    newguess

--- rd.py
+++ rd.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1, 100)
-while True:
+nu = 0
+while True:	
 	s = input("請猜數字")
 	s = int(s)
+	nu += 1 # nu = nu + 1
 	if s == r:
 		print("猜對瞜")
+		print("總共猜了", nu, "次")			
 		break
-	else:
-		if s > r:
-			print("再小一點")
-		if s < r:
-		    print("再大一點")
+	elif s > r:
+		print("再小一點")
+	elif s < r:
+		print("再大一點")
+	print("總共猜了", nu, "次")

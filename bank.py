581b2c6480ac8d46a5614f879f3fc39819fbbc0e
Raymond BBF <ketchup942@gmail.com>
2021-03-04 13:21:18 +0800

    bank_0

---
+++ bank.py
@@ -0,0 +1,45 @@
+while True:
+    password = input("請輸入密碼")
+    if password != "123456":
+        print("輸入錯誤1次,剩2次機會")
+        break
+    else:
+    	print("登入成功")
+    	raise SystemExit
+
+while True:
+    password = input("請輸入密碼")
+    if password != "123456":
+    	print("輸入錯誤2次,剩1次機會")
+    	break
+    else:
+    	print("登入成功")
+    	raise SystemExit
+    
+while True:
+    password = input("請輸入密碼")
+    if password != "123456":
+    	print("輸入錯誤3次,請洽詢櫃台")
+    	break
+    else:
+    	print("登入成功")
+    	raise SystemExit
+
+	    	
+
+
+
+
+
+
+
+        
+
+                      
+    
+            
+                    
+                          
+
+        
+    
